8d09655ec0c502b0e4e54150c73c98cdb345b2d8
Heavy108 <Heavy1089@gmail.com>
2024-08-03 21:46:07 +0530

    Services ui

--- alumn/src/app/Digital_Card/page.jsx
+++ alumn/src/app/Digital_Card/page.jsx
@@ -27,7 +27,7 @@ function Digital_AlumniCard() {
       <Title title="Services" />
       <center className={style.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</center>
       <div className={style.container4}>
-        <p></p>
+        
         <form action="" className={style.form}>
             <div className={style.half}>
           <h4>Digital Alumni Card</h4>
