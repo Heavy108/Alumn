d26d525e1b72db51f7f03f885d8ca601df4d53c8
Heavy108 <Heavy1089@gmail.com>
2024-08-09 17:31:57 +0530

    resolve import

--- alumn/src/app/api/Chat/route.js
+++ alumn/src/app/api/Chat/route.js
@@ -1,11 +1,11 @@
 //api/route.js
-import Ably from 'ably';
+import {Rest} from 'ably';
 
 
 export const revalidate = 0;
 
 export async function GET(request) {
-  const client = new Ably.Rest(process.env.ABLY_API_KEY);
+  const client = new Rest(process.env.ABLY_API_KEY);
   const tokenRequestData = await client.auth.createTokenRequest({
     clientId: 'ably-nextjs-demo',
   });
--- alumn/src/app/chat/page.jsx
+++ alumn/src/app/chat/page.jsx
@@ -1,10 +1,14 @@
 // app/chat/page.js
 'use client';
-import Chat from "@/components/ChatHelper";
+import Chat from "@/Components/ChatHelper";
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+// import dynamic from 'next/dynamic';
 
+// const Chat = dynamic(() => import('@/Components/ChatHelper.jsx'), {
+//   ssr: false,
+// });
 export default function ChatPage() {
   const { data: session, status } = useSession();
   const router = useRouter();
