f7fef0cf9a8d797bbb197aed855e094707ea7e84
Heavy108 <Heavy1089@gmail.com>
2024-09-17 21:36:51 +0530

    gallery magnify-pop up

--- alumn/src/Components/Gallery.jsx
+++ alumn/src/Components/Gallery.jsx
@@ -1,35 +1,88 @@
-import style from "@/css/Gallery.module.css";
+'use client'
+import { useState } from "react";
 import Image from "next/image";
 import Title from "./Title";
 import Button from "./Button";
-import sty from "@/css/button.module.css"
 import Link from "next/link";
+import style from "@/css/Gallery.module.css";
+import sty from "@/css/button.module.css";
 
+function Gallery(props) {
+  const imagePaths = Array.from({ length: props.len }, (_, i) => `/Gallery/${i + 1}.jpg`);
 
-function Gallery(props){
-    const imagePaths = Array.from({ length: props.len }, (_, i) => `/Gallery/${i + 1}.jpg`);
-    return(
-        <>
-        <Title title="Memories"/>
-        <div className={style.gallery_container}>
-        {imagePaths.map((path,index) => (
-            
-        <Image
-          key={path +  index}
-          src={path}
-          width={220}
-          height={220}
-          alt="Memories"
-        />
-      ))}
+  // State for lightbox
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-        </div>
-        <div className={sty.wrapper2}>
-        <Link href='/Gallery'><Button text="More" /></Link>
-      
+  // Function to open the lightbox on image click
+  const openLightbox = (index) => {
+    setCurrentImageIndex(index);
+    setIsOpen(true);
+  };
+
+  // Function to close the lightbox
+  const closeLightbox = () => {
+    setIsOpen(false);
+  };
+
+  // Function to move to the next image in the lightbox
+  const moveNext = () => {
+    setCurrentImageIndex((currentImageIndex + 1) % imagePaths.length);
+  };
+
+  // Function to move to the previous image in the lightbox
+  const movePrev = () => {
+    setCurrentImageIndex((currentImageIndex + imagePaths.length - 1) % imagePaths.length);
+  };
+
+  return (
+    <>
+      <Title title="Memories" />
+      <div className={style.gallery_container}>
+        {imagePaths.map((path, index) => (
+          <Image
+            key={path + index}
+            src={path}
+            width={220}
+            height={220}
+            alt="Memories"
+            className={style.gallery_image}
+            onClick={() => openLightbox(index)} // Open the lightbox on image click
+          />
+        ))}
       </div>
-      
-        </>
-    )
+
+      {/* Lightbox Implementation */}
+      {isOpen && (
+        <div className={style.lightbox_overlay}>
+          <div className={style.lightbox_content}>
+            <span className={style.close_button} onClick={closeLightbox}>
+              &times;
+            </span>
+            <Image
+              src={imagePaths[currentImageIndex]}
+              width={800}
+              height={600}
+              alt="Lightbox"
+              className={style.lightbox_image}
+            />
+            <button className={style.prev_button} onClick={movePrev}>
+              &#10094;
+            </button>
+            <button className={style.next_button} onClick={moveNext}>
+              &#10095;
+            </button>
+          </div>
+        </div>
+      )}
+
+      <div className={sty.wrapper2}>
+        <Link href="/Gallery">
+          <Button text="More" />
+        </Link>
+      </div>
+    </>
+  );
 }
+
 export default Gallery;
