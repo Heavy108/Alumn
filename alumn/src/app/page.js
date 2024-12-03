7174cd980ed493611f03fb73f7766b48f150b382
Heavy108 <Heavy1089@gmail.com>
2024-12-03 07:26:15 +0530

    test 7

--- alumn/src/app/page.js
+++ alumn/src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Home1 from "./Home/page";
-
+export const revalidate = 0;
 export default function Home() {
   return (
     <Home1/>
